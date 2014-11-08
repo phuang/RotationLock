ebd08611c250e34104b74b6d61649e911dad0499
cmotc <w4nder1n9w12ard+cmotc@gmail.com>
2014-11-08 01:49:37 -0500

    for some reason resetting USER_ROTATION to 0 and ACCELEROMETER_ROTATION to 1 doesn't re-enable auto rotation on my phone. Which is fine for me, but it's wrong and there's something I'm not seeing.

--- bin/RotationLock-release-unsigned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/RotationLock-release-unsigned.apk : \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/RotationLock.ap_ \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes.dex \
--- bin/classes.dex.d
+++
@@ -1,8 +0,0 @@
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes.dex : \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes/org/penghuang/tools/rotationlock/RotationLockActivity.class \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes/org/penghuang/tools/rotationlock/R.class \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes/org/penghuang/tools/rotationlock/RotationLockActivity$1.class \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes/org/penghuang/tools/rotationlock/R$drawable.class \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes/org/penghuang/tools/rotationlock/BuildConfig.class \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes/org/penghuang/tools/rotationlock/R$attr.class \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes/org/penghuang/tools/rotationlock/R$string.class \
