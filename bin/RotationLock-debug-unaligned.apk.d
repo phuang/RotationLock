f34d338dd81befbf7a8c92e9c862c392c3243da9
cmotc <cmotc@openmailbox.org>
2015-01-24 19:55:07 -0500

    changed name of App/package, API level 10 instead of 20,

--- bin/RotationLock-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/RotationLock-debug-unaligned.apk : \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/RotationLock.ap_ \
-/home/devhost/android/PersonalROM-schS738c/RotationLock/bin/classes.dex \
